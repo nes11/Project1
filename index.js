3d3caa1a2355d907f162163a0c999720f7bb5126
patcheko <patcheko@MacBook-Air-de-patcheko.local>
2018-09-05 19:30:33 +0100

    rockpaperscissors

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-const port = 4000
+const port = 4000;
 
-app.get('/', function (req, res) {
-  console.log('/ has been visited');
-  res.send('Hello World');
-})
+const links = '<a href="/game?player1=rock">rock</a><br /><a href="/game?player1=scissors">scissors</a><br /><a href="/game?player1=paper">paper</a>'
 
-app.get('/blah', function (req, res) {
-  const query = req.query;
-  console.log('/blah has been visited');
-  console.log("Query is:", query);
-  const responseText = 'Fuck you world ' + query.name;
-  res.send(responseText);
-  console.log('Responded with:', responseText)
+const makeComputerChoice = () => {
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 1) {
+      computerChoice = 'rock';
+  } else if (computerChoice === 2) {
+      computerChoice = 'paper';
+  } else {
+      computerChoice = 'scissors';
+  }
+  console.log('The computer has chosen', computerChoice);
+  return computerChoice;
+}
+
+const play = function(userChoice, computerPlay) {
+  if (userChoice === computerPlay) {
+      return "It's a draw!";
+  } else if (userChoice === 'paper') {
+      if (computerPlay === 'rock') {
+        return 'You win!';
+    } else if (computerPlay === 'scissors') {
+        return 'The computer wins!';
+    }
+  } else if (userChoice === 'rock') {
+      if (computerPlay === 'paper') {
+        return 'The computer wins!';
+    } else if (computerPlay === 'scissors') {
+        return 'You win!';
+    }
+  } else if (userChoice === 'scissors') {
+    if (computerPlay === 'rock') {
+      return 'The computer wins';
+    } else if (computerPlay === 'paper') {
+      return 'You win!';
+    }
+  }
+}
+
+app.get('/game', function (req, res) {
+  const computerChoice = makeComputerChoice();
+  const playerChoice = req.query.player1;
+  const result = play(playerChoice, computerChoice);
+  const infoGame = `You have chosen ${playerChoice} and the computer has chosen ${computerChoice}.<br /> ${result}`;
+  const responseString = `${infoGame}<br />${links}`;
+  res.send(responseString);
 })
 
 app.listen(port, function(){ console.log('this application is listening on port', port)});
